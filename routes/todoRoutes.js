0ad43b793826ca6364786773ffd8fc7cd61d9ee3
Erika natalia <eri-martin13@hotmail.com>
2022-03-02 16:14:33 -0500

    Delete function with rest

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const todo = require("../models/todo");
+const mongoose = require("mongoose");
+
 /* let todo = [
     {
       "id": 1,
@@ -18,12 +20,22 @@ const todo = require("../models/todo");
     }
 ] */
 
+
 router.get("/", (req, response) => {
-    response.send('<h1>Todo List "Flores El Tambo"</h1>');
+  response.send('<h1>Todo List "Flores El Tambo"</h1>');
 });
 router.get("/api/todo", async (req, response) => {
-    const resp = await todo.find();
-    response.status(200).json(resp);
+  const resp = await todo.find();
+  response.json(resp);
+});
+
+
+/* prueba */
+router.delete("/api/todo/:id", async (req, response) => {
+  const id = req.params["id"]
+  await todo.findOneAndRemove({ id });
+  response.status(204).json({})
+  console.log(id);
 });
 
 module.exports = router;
