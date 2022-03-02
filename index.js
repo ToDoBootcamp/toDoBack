a7b49c9d05a2ac23b58848c4251c3409421f7457
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-03-01 21:51:02 -0500

    start project

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World')
+let todo = [
+  {
+    "id": 1,
+    "title": 'Test ToDo',
+    "description": 'First Test',
+    "date": new Date(),
+    "state": 'completed',
+    "deleted": false
+  },{
+    "id": 2,
+    "title": 'Test ToDo 2',
+    "description": 'Second Test',
+    "date": new Date(),
+    "state": 'incompleted',
+    "deleted": false
+  }
+]
+
+// Endpoints
+app.get('/', (request, response) => {
+  response.send('<h1>Todo List "Flores El Tambo"</h1>')
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.get('/api/todo', (request, response) => {
+  response.json(todo)
+})
+
+const PORT = 3001 // port where the server runs
+app.listen(PORT, () => { // server started asynchronously
+  console.log(`Server running on port ${PORT}`)
+})
