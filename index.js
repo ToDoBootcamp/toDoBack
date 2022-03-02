2a2c7b57046b398a0bd40451841446d0614482c2
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-03-02 15:08:36 -0500

    cors policy added env port added

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const app = express();
-const todoRoutes = require("./routes/todoRoutes");
-const mongoose = require("mongoose");
+const express = require('express')
+const app = express()
+const todoRoutes = require("./routes/todoRoutes")
+const mongoose = require("mongoose")
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
 
 // Endpoints
 
-app.use("/", todoRoutes);
-app.use("/api/todo", todoRoutes);
+app.use("/", todoRoutes)
+app.use("/api/todo", todoRoutes)
+app.use("api/todo/:id", todoRoutes)
 
 mongoose.connect("mongodb+srv://floresDelTambo:floresDelTambo123@florestambo.4eyff.mongodb.net/todoList?retryWrites=true&w=majority")
   .then(() => console.log("Connected successfully")).catch((err) => console.error(err));
 
+// Error 404
+app.use((req, response) => {
+  response.status(404).json({
+    error: 'Not Found'
+  })
+})
 
-const PORT = 3001 // port where the server runs
+const PORT = process.env.PORT || 3001 // port where the server runs
 app.listen(PORT, () => { // server started asynchronously
-  console.log(`Server running on http:/localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`)
 })
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,29 +1,36 @@
-const router = require("express").Router();
-const todo = require("../models/todo");
-/* let todo = [
-    {
-      "id": 1,
-      "title": 'Test ToDo',
-      "description": 'First Test',
-      "date": new Date(),
-      "state": 'completed',
-      "deleted": false
-    },{
-      "id": 2,
-      "title": 'Test ToDo 2',
-      "description": 'Second Test',
-      "date": new Date(),
-      "state": 'incompleted',
-      "deleted": false
-    }
-] */
+const router = require("express").Router()
+const res = require("express/lib/response");
+const todo = require("../models/todo")
 
 router.get("/", (req, response) => {
     response.send('<h1>Todo List "Flores El Tambo"</h1>');
-});
+})
+
 router.get("/api/todo", async (req, response) => {
     const resp = await todo.find();
     response.status(200).json(resp);
-});
+})
+
+router.put("/api/todo/:id", (req, response) =>{
+  const id = Number(req.params.id)
+  const body = req.body
+
+  const updateTodo = {
+    "id": id,
+    "title": body.title,
+    "description": body.description,
+    "date": new Date().toISOString(),
+    "state": body.state,
+    "deleted": body.deleted,
+  }
+
+  todo.findOneAndUpdate({id: id}, updateTodo, (err, result) => {
+    if(err) throw new Error(err)
+    res.json(result)
+  })
+
+  console.log({ newTodo: updateTodo })
+  // response.json(updateTodo)
+})
 
 module.exports = router;
