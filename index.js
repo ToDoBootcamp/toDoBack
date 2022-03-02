922992eb65265cd973ae24b88cff2bcdf735bdb0
Christian Montoya <chrimonlo@gmail.com>
2022-03-02 16:12:20 -0500

    Create functionality added

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const todoRoutes = require("./routes/todoRoutes");
 const mongoose = require("mongoose");
-
+app.use(express.json());
 // Endpoints
 
 app.use("/", todoRoutes);
 app.use("/api/todo", todoRoutes);
+app.use("/new", todoRoutes);
 
 mongoose.connect("mongodb+srv://floresDelTambo:floresDelTambo123@florestambo.4eyff.mongodb.net/todoList?retryWrites=true&w=majority")
   .then(() => console.log("Connected successfully")).catch((err) => console.error(err));
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,29 +1,27 @@
 const router = require("express").Router();
+
 const todo = require("../models/todo");
-/* let todo = [
-    {
-      "id": 1,
-      "title": 'Test ToDo',
-      "description": 'First Test',
-      "date": new Date(),
-      "state": 'completed',
-      "deleted": false
-    },{
-      "id": 2,
-      "title": 'Test ToDo 2',
-      "description": 'Second Test',
-      "date": new Date(),
-      "state": 'incompleted',
-      "deleted": false
-    }
-] */
+/* endpoints */
 
 router.get("/", (req, response) => {
     response.send('<h1>Todo List "Flores El Tambo"</h1>');
 });
+/*endpoint to show all the todos */
 router.get("/api/todo", async (req, response) => {
     const resp = await todo.find();
-    response.status(200).json(resp);
+    response.json(resp);
 });
+/* create a document with the information of the request */
+router.post("/new", (req, response) => {
+  try {
+    console.log(req.body);
+    todo.create(req.body);
+    response.status(201);
+  } catch (err){ 
+    response.status(500);
+    
+  }
+  
+})
 
 module.exports = router;
