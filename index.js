85f8436d868d500a2d987c8359c92ab0c8bc83fa
Christian Montoya <chrimonlo@gmail.com>
2022-03-02 02:32:22 -0500

    conection database and back end

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
-const express = require('express')
-const app = express()
-
-let todo = [
-  {
-    "id": 1,
-    "title": 'Test ToDo',
-    "description": 'First Test',
-    "date": new Date(),
-    "state": 'completed',
-    "deleted": false
-  },{
-    "id": 2,
-    "title": 'Test ToDo 2',
-    "description": 'Second Test',
-    "date": new Date(),
-    "state": 'incompleted',
-    "deleted": false
-  }
-]
+const express = require('express');
+const app = express();
+const todoRoutes = require("./routes/todoRoutes");
+const mongoose = require("mongoose");
 
 // Endpoints
-app.get('/', (request, response) => {
 
+app.use("/", todoRoutes);
+app.use("/api/todo", todoRoutes);
+
+mongoose.connect("mongodb+srv://products:products123@cluster0.demhi.mongodb.net/todoList?retryWrites=true&w=majority")
+  .then(() => console.log("Connected successfully")).catch((err) => console.error(err));
+
+/* app.get('/', (request, response) => {
   response.send('<h1>Todo List "Flores El Tambo"</h1>')
-  
-})
+}) */
 
-app.get('/api/todo', (request, response) => {
+/* app.get('/api/todo', (request, response) => {
   response.json(todo)
-})
+}) */
 
 const PORT = 3001 // port where the server runs
 app.listen(PORT, () => { // server started asynchronously
---
+++ routes/todoRoutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const todo = require("../models/todo");
+/* let todo = [
+    {
+      "id": 1,
+      "title": 'Test ToDo',
+      "description": 'First Test',
+      "date": new Date(),
+      "state": 'completed',
+      "deleted": false
+    },{
+      "id": 2,
+      "title": 'Test ToDo 2',
+      "description": 'Second Test',
+      "date": new Date(),
+      "state": 'incompleted',
+      "deleted": false
+    }
+] */
+
+router.get("/", (req, response) => {
+    response.send('<h1>Todo List "Flores El Tambo"</h1>');
+});
+router.get("/api/todo", async (req, response) => {
+    const resp = await todo.find();
+    response.json(resp);
+})
+
+module.exports = router;
