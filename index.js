bdbd05d4495858656b553fe194ff109b07b0e257
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-03-02 11:46:53 -0500

    pruebas

--- index.js
+++ index.js
@@ -8,18 +8,12 @@ const mongoose = require("mongoose");
 app.use("/", todoRoutes);
 app.use("/api/todo", todoRoutes);
 
-mongoose.connect("mongodb+srv://products:products123@cluster0.demhi.mongodb.net/todoList?retryWrites=true&w=majority")
-  .then(() => console.log("Connected successfully")).catch((err) => console.error(err));
+mongoose.connect("mongodb+srv://floresDelTambo:floresDelTambo123@florestambo.4eyff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+  .then(() => console.log("Connected successfully"))
+  .catch((err) => console.error(err));
 
-/* app.get('/', (request, response) => {
-  response.send('<h1>Todo List "Flores El Tambo"</h1>')
-}) */
-
-/* app.get('/api/todo', (request, response) => {
-  response.json(todo)
-}) */
 
 const PORT = 3001 // port where the server runs
 app.listen(PORT, () => { // server started asynchronously
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on http:/localhost:${PORT}`)
 })
